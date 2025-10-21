6b403b1340a6a47b3e509f9c0ab0633ad2f52941
Daniel-Cdemy <daniel_martin@teilnehmer.cdemy.de>
2025-10-21 13:02:15 +0200

    Finish oop04

--- main/bin/main.dart
+++ main/bin/main.dart
@@ -1,33 +1,16 @@
-import 'package:main/enums.dart';
 import 'package:main/teilnehmer.dart';
 
 void main(List<String> arguments) {
-  final participant = Participant(
-    isParticipant: YesNo.yes,
+  final studentA = Student(
     firstName: 'Max',
-    lastName: 'Mustermann',
-    age: 25,
-    adress: 'Musterstraße 1',
+    lastName: 'Muster',
     gender: Gender.male,
-    interExam: YesNo.yes,
-    passedInterExam: YesNo.no,
-    finalExam: YesNo.no,
-    passedFinalExam: YesNo.no,
   );
-
-  print('Die Person');
-  print('Ist ein Teilnehmer: ${yesNoToText(participant.isParticipant)}');
-  print('Vorname: ${participant.firstName}');
-  print('Nachname: ${participant.lastName}');
-  print('Alter: ${participant.age}');
-  print('Adresse: ${participant.adress}');
-  print('Geschlecht: ${genderToText(participant.gender)}');
-  print('Zwischenprüfung geschrieben?: ${yesNoToText(participant.interExam)}');
-  print(
-    'Zwischenprüfung bestanden?: ${yesNoToText(participant.passedInterExam)}',
+  final studentB = Student(
+    firstName: 'Bibi',
+    lastName: 'Meier',
+    gender: Gender.female,
   );
-  print('Abschlussprüfung geschrieben?: ${yesNoToText(participant.finalExam)}');
-  print(
-    'Abschlussprüfung bestanden?: ${yesNoToText(participant.passedFinalExam)}',
-  );
+  final institution = Institution('Cdemy', [studentA, studentB]);
+  print(institution.students.length);
 }
--- main/lib/teilnehmer.dart
+++ main/lib/teilnehmer.dart
@@ -1,27 +1,44 @@
-import 'package:main/enums.dart';
+import 'dart:math';
 
-class Participant {
-  YesNo isParticipant;
-  String firstName;
-  String lastName;
-  Gender gender;
-  int? age;
-  String? adress;
-  YesNo interExam;
-  YesNo passedInterExam;
-  YesNo finalExam;
-  YesNo passedFinalExam;
+enum Gender { male, female, diverse }
 
-  Participant({
-    required this.isParticipant,
+String genderToText(Gender gender) {
+  switch (gender) {
+    case Gender.male:
+      return 'Männlich';
+    case Gender.female:
+      return 'Weiblich';
+    case Gender.diverse:
+      return 'Divers';
+  }
+}
+
+class Institution {
+  final String name;
+  final List<Student> students;
+  Institution(this.name, this.students);
+}
+
+class Authorization {
+  final random = Random();
+  int? _id;
+  int get id {
+    if (_id != null) return _id!;
+    _id = random.nextInt(1000000000);
+    return _id!;
+  }
+}
+
+class Student {
+  final Authorization _authorization = Authorization();
+  final String firstName;
+  final String lastName;
+  final Gender gender;
+  int get id => _authorization.id;
+
+  Student({
     required this.firstName,
     required this.lastName,
     this.gender = Gender.diverse,
-    this.age,
-    this.adress,
-    this.interExam = YesNo.no,
-    this.finalExam = YesNo.no,
-    this.passedInterExam = YesNo.no,
-    this.passedFinalExam = YesNo.no,
   });
 }
